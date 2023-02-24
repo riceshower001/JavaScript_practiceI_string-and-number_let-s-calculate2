accc6943d94ce21692ebfa3d66a8e0808ce73877
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-02-25 04:14:07 +0900

    Add files via upload

---
+++ JavaScript_practiceI_string and number_let's calculate2.js
@@ -0,0 +1,8 @@
+// Please print the result of multiplying 8 by 4.
+console.log(8*4);
+
+// Please print the result of dividing 24 by 4.
+console.log(24/4);
+
+// Please print the remainder of dividing 7 by 2.
+console.log(7%2);
